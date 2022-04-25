57aa3bff7b56d47e2c3142219c4828dcf1e7d3f3
swilson <21scott.wilson@gmail.com>
2022-04-25 23:53:06 +0100

    'Extended brain-pairs and script corrections'

--- script_conn.py
+++ script_conn.py
@@ -22,6 +22,7 @@ from doctest import NORMALIZE_WHITESPACE
 from copy import deepcopy
 from ipywidgets import IntProgress
 import json
+from itertools import chain
 
 
 logger = logging.getLogger(__name__)
@@ -40,6 +41,17 @@ rm = pymaid.CatmaidInstance(**creds)
 
 ### LR connectivity analysis ###
 
+
+
+#%%
+METRIC = "cosine"
+
+bp = pd.read_csv(HERE / "brain-pairs.csv")
+subsetbp = bp[bp.region.isna()]
+subsetbp.drop('region', axis=1, inplace=True)
+skid_pairs = subsetbp.to_numpy().astype(int).tolist()
+bp.drop('region', axis=1, inplace=True)
+brain_pairs = bp.to_numpy().astype(int).tolist()
 
 
 ## Define functions
@@ -62,17 +74,20 @@ def merged_analysis(pair_list, metric = "cosine", is_input = False):
     -------
         pd.Data Frame: rows = # of pairs (1172), 3 columns (left neuron, right neuron, cosine similarity score)
     """    
-    out_file = OUT_DIR / f"canalysis_{metric}_{'in' if is_input else 'out'}put.json"
-    if out_file.is_file():
-        with open(out_file) as f:
-            return json.load(f)
+    # out_file = OUT_DIR / f"canalysis_{metric}_{'in' if is_input else 'out'}put.json"
+    # if out_file.is_file():
+    #     with open(out_file) as f:
+    #         return json.load(f)
 
     pair_dict = {}
-    for pair in pair_list:
-        pair_dict[pair[0]] = pair[1]
-        pair_dict[pair[1]] = pair[0]
-    
+    for skids in brain_pairs:
+        pair_dict[skids[0]] = skids[1]
+        pair_dict[skids[1]] = skids[0]
+    # Construct dictionary of all brain pairs (from CSV)
+
+
     sims = []
+    # For each pair iteratively, obtain all partners and filter to get just input or outputs (i/o)
     for pair in pair_list:
         partners = pymaid.get_partners(list(pair))
         if is_input == True:
@@ -83,95 +98,65 @@ def merged_analysis(pair_list, metric = "cosine", is_input = False):
         partner_skids = set(partners["skeleton_id"])
 
         sum0 = float(sum(partners.iloc[:, 4]))
+        if sum0 == 0:
+            sims.append(np.NaN)
         sum1 = float(sum(partners.iloc[:, 5]))
+        if sum1 == 0:
+            sims.append(np.NaN)
+        # If a sum of synaptic counts for pair's i/o connections is 0, return Na for connectivity score
+        if sum0 and sum1 != 0:
 
-        column0 = []
-        column1 = []
-        while partner_skids:
-            partner = partner_skids.pop()
-            partner_homolog = pair_dict.get(partner) #partner_homolog is the L-R pair of partner
+            column0 = []
+            column1 = []
+            # Iteratively go through partners, and apply merging and normalization protocol
+            while partner_skids:
+                partner = partner_skids.pop()
+                partner_homolog = pair_dict.get(partner) # Partner_homolog is the L-R pair of partner
+                
+                if partner_homolog is None:
+                    # No L-R pair to merge, but must normalize (partner:L/R-pair synapses ÷ sum L/R synapses)
+                    col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] / sum0
+                    column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+                    # try:
+                    col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] / sum1
+                    # except ZeroDivisionError:
+                    #     col1 = 0
+                    column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+                else:
+                    # Merge the two vectors (L-R pairs) and normalize ()
+                    col0 = (partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])].values + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])].values) / sum0
+                    column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+                    col1 = (partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])].values + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])].values) / sum1
+                    column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+                
+                # Remove both from set, before iterating to next partner
+                partner_skids.discard(partner)
+                partner_skids.discard(partner_homolog)   
 
-            """ if partner_homolog is None:
-                # Nothing to fuse, but must normalize
-                col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])].values / sum0
-                column0.append(pd.to_numeric(col0, errors = 'coerce'))
-                col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])].values / sum1
-                column1.append(pd.to_numeric(col1, errors = 'coerce'))
-                print(type(col0))
-                print(type(col1))
-            else:
-                # Fuse two vectors, normalized
-                col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])].values + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])].values / sum0
-                column0.append(pd.to_numeric(col0, errors = 'coerce'))
-                col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])].values + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])].values / sum1
-                column1.append(pd.to_numeric(col1, errors = 'coerce')) """
-            
-            if partner_homolog is None:
-                # Nothing to fuse, but must normalize
-                col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] / sum0
-                column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
-                # try:
-                col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] / sum1
-                # except ZeroDivisionError:
-                #     col1 = 0
-                column1.append(pd.to_numeric(col1, errors = 'coerce'))
-            else:
-                # Fuse two vectors, normalized
-                col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])] / sum0
-                column0.append(pd.to_numeric(col0, errors = 'coerce'))
-                col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])] / sum1
-                column1.append(pd.to_numeric(col1, errors = 'coerce'))
-            
-            # Remove both from set
-            partner_skids.discard(partner)
-            partner_skids.discard(partner_homolog)          
-        # Two rows, as many columns as were needed
-        column0 = pd.Series(column0).values
-        column1 = pd.Series(column1).values
+            # Obtain columns of merged/normalized values (initially as ndarray of 1-item ndarrays) and convert into adjacency matrix
+            column0 = pd.Series(column0).values
+            column1 = pd.Series(column1).values
 
-        flat_col0 = []
-        for sublist in column0:
-            for item in sublist:
-                flat_col0.append(item)
+            flat_col0 = np.array(list(chain.from_iterable(column0)))
+            flat_col1 = np.array(list(chain.from_iterable(column1)))
+            matrix = pd.DataFrame([flat_col0, flat_col1]) # rows have to be pair_L, pair_R
 
-        flat_col1 = []
-        for sublist in column1:
-            for item in sublist:
-                flat_col1.append(item)
+            # Calculate connectivity score for pair and append 
+            scores = navis.connectivity_similarity(adjacency = matrix, metric = metric, n_cores= 1)
+            sims.append(scores.iloc[0][1])
 
-        matrix = pd.DataFrame([flat_col0, flat_col1]) # rows have to be pair_L, pair_R
-        scores = navis.connectivity_similarity(adjacency = matrix, metric = metric, n_cores= 1)
-        sims.append(scores.iloc[0][1])
-    
     print(sims)
-
-
-METRIC = "cosine"
-
-bp = pd.read_csv(HERE / "brain-pairs.csv")
-bp.drop('Unnamed: 0', axis=1, inplace=True)
-# left_skids = list(bp["left"])
-# right_skids = list(bp["right"])
-
-skid_pairs = bp.to_numpy().astype(int).tolist()
-
-#%%
-merged_input = merged_analysis(skid_pairs, is_input = True)
-
-print(merged_input)
-
-sys.exit()
-
-#%%
-    # Output scores as pd.DF, int columns for skids & float for scores 
-    # out = pd.DataFrame(pair_list, dtype=int, columns=["left_skid", "right_skid"])
-    # out["cosine_similarity"] = pd.Series(np.asarray(sims, dtype=float))
+            
+        # Output scores as pd.DF, int columns for skids & float for scores 
+    out = pd.DataFrame(pair_list, dtype=int, columns=["left_skid", "right_skid"])
+    out["cosine_similarity"] = pd.Series(np.asarray(sims, dtype=float))
 
     # with open(out_file, "w") as f:
-    #     out_json = out.to_json()
-    #     json.dump(out_json, f, indent=2, sort_keys=True)
+    #      out_json = out.to_json()
+    #      json.dump(out_json, f, indent=2, sort_keys=True)
 
-    # return out
+    return out
+    
 
 
 ## Run analysis and explore data ##
@@ -181,16 +166,12 @@ sys.exit()
 METRIC = "cosine"
 
 bp = pd.read_csv(HERE / "brain-pairs.csv")
-bp.drop('Unnamed: 0', axis=1, inplace=True)
-# left_skids = list(bp["left"])
-# right_skids = list(bp["right"])
-
+bp = bp[bp.region.isna()]
+bp.drop('region', axis=1, inplace=True)
 skid_pairs = bp.to_numpy().astype(int).tolist()
 
 #%%
 merged_input = merged_analysis(skid_pairs, is_input = True)
-print(merged_input)
-
 merged_input.to_csv(OUT_DIR / "merged_threshold_cosine_input.csv", index=False)
 
 #%%
@@ -199,6 +180,60 @@ merged_output.to_csv(OUT_DIR / "merged_threshold_cosine_output.csv", index=False
 
 
 sys.exit()
+
+## run for specific pair
+partners = pymaid.get_partners(list(pair))
+partners = partners[partners.relation == 'upstream']
+partner_skids = set(partners["skeleton_id"])
+sum0 = float(sum(partners.iloc[:, 4]))
+sum1 = float(sum(partners.iloc[:, 5]))
+column0 = []
+column1 = []
+while partner_skids:
+    partner = partner_skids.pop()
+    partner_homolog = pair_dict.get(partner) 
+
+    if partner_homolog is None:
+        col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] / sum0
+        column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+        col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] / sum1
+        column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+    else:
+        col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])] / sum0
+        column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+        col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])] / sum1
+        column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+
+    partner_skids.discard(partner)
+    partner_skids.discard(partner_homolog) 
+
+column0 = pd.Series(column0).values
+column1 = pd.Series(column1).values
+flat_col0 = np.array(list(chain.from_iterable(column0)))
+flat_col1 = np.array(list(chain.from_iterable(column1)))
+matrix = pd.DataFrame([flat_col0, flat_col1])
+print(matrix)
+score = navis.connectivity_similarity(adjacency = matrix, metric = 'cosine', n_cores= 1)
+score = score.iloc[0][1]
+print(score)
+
+
+## Specific col calculations
+
+partner = 
+partner_homolog = pair_dict.get(partner)
+                
+if partner_homolog is None:
+    col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] / sum0
+    column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+    col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] / sum1
+    column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+else:
+    col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])] / sum0
+    column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
+    col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])] / sum1
+    column1.append(pd.to_numeric(col1.values, errors = 'coerce'))
+
 
 ## old analysis code to revamp
 
@@ -232,75 +267,3 @@ ax.set_xlabel(f"{METRIC} similarity value")
 ax.set_label("Cumulative frequency")
 
 plt.show()
-# %%
-
-pair_dict = {}
-for pair in skid_pairs:
-    pair_dict[pair[0]] = pair[1]
-    pair_dict[pair[1]] = pair[0]
-
-sims = []
-for pair in skid_pairs[0:10]:
-    partners = pymaid.get_partners(list(pair))
-    partners = partners[partners.relation == 'upstream']
-    partner_skids = set(partners["skeleton_id"])
-
-    sum0 = float(sum(partners.iloc[:, 4]))
-    sum1 = float(sum(partners.iloc[:, 5]))
-
-    column0 = []
-    column1 = []
-    while partner_skids:
-        partner = partner_skids.pop()
-        partner_homolog = pair_dict.get(partner) 
-            
-        if partner_homolog is None:
-            col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] / sum0
-            column0.append(pd.to_numeric(col0.values, errors = 'coerce'))
-            try:
-                col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] / sum1
-            except ZeroDivisionError:
-                col1 = 0
-                column1.append(pd.to_numeric(col1, errors = 'coerce'))
-        else:
-                # Fuse two vectors, normalized
-            col0 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[0])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[0])] / sum0
-            column0.append(pd.to_numeric(col0, errors = 'coerce'))
-            col1 = partners.loc[partners['skeleton_id'] == str(partner)][str(pair[1])] + partners.loc[partners['skeleton_id'] == str(partner_homolog)][str(pair[1])] / sum1
-            column1.append(pd.to_numeric(col1, errors = 'coerce'))
-            
-            # Remove both from set
-        partner_skids.discard(partner)
-        partner_skids.discard(partner_homolog)          
-        # Two rows, as many columns as were needed
-    column0 = pd.Series(column0).values
-    column1 = pd.Series(column1).values
-
-    flat_col0 = []
-    for sublist in column0:
-        for item in sublist:
-            flat_col0.append(item)
-
-    flat_col1 = []
-    for sublist in column1:
-        for item in sublist:
-            flat_col1.append(item)
-        
-
-    matrix = pd.DataFrame([flat_col0, flat_col1]) # rows have to be pair_L, pair_R
-    print(matrix)
-        # # rows have to be pair_L, pair_R
-    if __name__ == '__main__':
-        sims = navis.connectivity_similarity(adjacency = matrix, metric = "cosine", threshold = 2)
-        sims.append(sims)
-
-
-print(sims)
-
-    # Output scores as pd.DF, int columns for skids & float for scores 
-out = pd.DataFrame(pair_list, dtype=int, columns=["left_skid", "right_skid"])
-out["cosine_similarity"] = pd.Series(np.asarray(sims, dtype=float))
-
-
-
-# %%
